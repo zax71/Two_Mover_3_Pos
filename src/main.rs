eb24dfec5ade031d1125506767b17d5317582688
Zax71 <67716263+zax71@users.noreply.github.com>
2025-07-14 18:33:13 +0100

    Name the window title

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Two Mover 3 Pos",
         native_options,
         Box::new(|cc| Ok(Box::new(two_mover_3_pos::App::new(cc)))),
     )
