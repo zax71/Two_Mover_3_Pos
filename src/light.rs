9ff0d13b28b2df0009c7d3fd0a21ec465fec6216
Zax71 <67716263+zax71@users.noreply.github.com>
2025-09-16 18:55:04 +0100

    Fix light::point_at()

--- src/light.rs
+++ src/light.rs
@@ -27,6 +27,7 @@ impl IsDefault for Light {
 
 impl Light {
     /// Calculates the required pan and tilt for this light to point at the specified point in space
+    /// coordinates in form (x,y,z) with z being up, x right and y forward
     pub fn point_at(&self, coordinate: Vector3d<f64>) -> LightState {
         // Desmos link: https://www.desmos.com/3d/ez2rjf9ahn (We're working with y up, this is working with z up fyi)
 
@@ -37,10 +38,16 @@ impl Light {
         let distance_straight = (distance_x.powi(2) + distance_y.powi(2)).sqrt();
 
         // Now some trig with those values to calculate the pan and tilt (in degrees)
-        // Take 90 degrees from the pan as zero is towards -x by default but we want towards +z
         // Adding f64::MIN_POSITIVE to stop divide by zero errors
-        let pan = ((distance_y / distance_x + f64::MIN_POSITIVE).atand()) - 90.0;
-        let tilt = (distance_straight / distance_z + f64::MIN_POSITIVE).atand();
+        let mut pan =
+            (coordinate.x - self.coordinates.x).atan2d(&(coordinate.y - self.coordinates.y));
+        let tilt = ((distance_straight / distance_z + f64::MIN_POSITIVE).atand()) * -1.0; // * -1 as it is always -ve
+
+        // The pan can be -ve and when that occurs it is taking the reading anticlockwise. Let's add 180 to the positive version of the pan to fix it
+        if pan < 0.0 {
+            pan = pan * -1.0; // make positive
+            pan = pan + 180.0;
+        }
 
         LightState {
             pan,
@@ -61,13 +68,21 @@ mod tests {
         diff < 0.000001 && diff > -0.000001
     }
 
+    fn eq_light_state(state_1: LightState, state_2: LightState) -> bool {
+        let address_eq = state_1.address == state_2.address;
+        let pan_eq = float_eq(state_1.pan, state_2.pan);
+        let tilt_eq = float_eq(state_1.tilt, state_2.tilt);
+
+        address_eq && pan_eq && tilt_eq
+    }
+
     #[test]
     fn test_light_point_at_1() {
         let light = Light {
             coordinates: Vector3d {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
+                x: 6.0,
+                y: 4.0,
+                z: 1.0,
             },
             minimum_beam: 0,
             maximum_beam: 0,
@@ -76,14 +91,14 @@ mod tests {
         };
 
         let point = Vector3d {
-            x: 0.0,
-            y: 0.0,
+            x: 6.0,
+            y: 3.0,
             z: 0.0,
         };
 
         let out_light_state = LightState {
-            pan: -90.0,
-            tilt: 90.0,
+            pan: 180.0,
+            tilt: 45.0,
             address: 5,
         };
 
@@ -94,9 +109,9 @@ mod tests {
     fn test_light_point_at_2() {
         let light = Light {
             coordinates: Vector3d {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
+                x: 6.0,
+                y: 4.0,
+                z: 1.0,
             },
             minimum_beam: 0,
             maximum_beam: 0,
@@ -105,17 +120,75 @@ mod tests {
         };
 
         let point = Vector3d {
-            x: 0.0,
-            y: 0.0,
+            x: 7.0,
+            y: 4.0,
             z: 0.0,
         };
 
         let out_light_state = LightState {
-            pan: -90.0,
+            pan: 90.0,
             tilt: 45.0,
             address: 5,
         };
 
         assert_eq!(light.point_at(point), out_light_state)
+    }
+
+    #[test]
+    fn test_light_point_at_3() {
+        let light = Light {
+            coordinates: Vector3d {
+                x: 6.0,
+                y: 4.0,
+                z: 1.0,
+            },
+            minimum_beam: 0,
+            maximum_beam: 0,
+            name: "N/A".to_string(),
+            address: 5,
+        };
+
+        let point = Vector3d {
+            x: 5.0,
+            y: 3.0,
+            z: 0.0,
+        };
+
+        let out_light_state = LightState {
+            pan: 315.0,
+            tilt: 54.7356103172,
+            address: 5,
+        };
+
+        assert!(eq_light_state(light.point_at(point), out_light_state));
+    }
+
+    #[test]
+    fn test_light_point_at_4() {
+        let light = Light {
+            coordinates: Vector3d {
+                x: -3.2,
+                y: -1.8,
+                z: 6.0,
+            },
+            minimum_beam: 0,
+            maximum_beam: 0,
+            name: "N/A".to_string(),
+            address: 5,
+        };
+
+        let point = Vector3d {
+            x: -4.0,
+            y: -2.8,
+            z: -2.47,
+        };
+
+        let out_light_state = LightState {
+            pan: 321.340191746,
+            tilt: 8.59773680459,
+            address: 5,
+        };
+
+        assert!(eq_light_state(light.point_at(point), out_light_state));
     }
 }
